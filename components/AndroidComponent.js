5b8a7c2004e5741f910e7f32256928435a81ac21
Khoa Pham <onmyway133@gmail.com>
2017-08-15 16:26:31 +0200

    Begin to support iOS and Android

---
+++ components/AndroidComponent.js
@@ -0,0 +1,10 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+class AndroidComponent extends React.Component {
+  render() {
+    return React.createElement('div', {})
+  }
+}
+
+module.exports = AndroidComponent
