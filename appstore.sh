4bced3677ff4d8ee70a095c4aaf81441e427fa51
Khoa Pham <onmyway133@gmail.com>
2019-08-01 00:02:42 +0200

    Use no space

--- appstore.sh
+++ appstore.sh
@@ -1,3 +1,3 @@
-npx electron-packager . "Push Notification Tester" --app-bundle-id=com.onmyway133.PushNotifications --helper-bundle-id=com.onmyway133.PushNotifications.helper --app-version=1.4.0 --build-version=1.0.100 --platform=mas --arch=x64 --icon=Icon/Icon.icns --overwrite
-npx electron-osx-sign "Push Notification Tester-mas-x64/Push Notification Tester.app" --verbose
-npx electron-osx-flat "Push Notification Tester-mas-x64/Push Notification Tester.app" --verbose
+npx electron-packager . "PushNotificationTester" --app-bundle-id=com.onmyway133.PushNotifications --helper-bundle-id=com.onmyway133.PushNotifications.helper --app-version=1.4.0 --build-version=1.0.100 --platform=mas --arch=x64 --icon=Icon/Icon.icns --overwrite
+npx electron-osx-sign "PushNotificationTester-mas-x64/PushNotificationTester.app" --verbose
+npx electron-osx-flat "PushNotificationTester-mas-x64/PushNotificationTester.app" --verbose
