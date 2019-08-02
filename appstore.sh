077ed44e1e54c4d8ae175b540c6b8afb058fb643
Khoa Pham <onmyway133@gmail.com>
2019-08-02 15:06:08 +0200

    Use version

--- appstore.sh
+++ appstore.sh
@@ -1,3 +1,3 @@
-npx electron-packager . "PushNotificationTester" --app-bundle-id=com.onmyway133.PushNotifications --helper-bundle-id=com.onmyway133.PushNotifications.helper --app-version=1.4.0 --build-version=1.0.100 --platform=mas --arch=x64 --icon=Icon/Icon.icns --overwrite
+npx electron-packager . "PushNotificationTester" --app-bundle-id=com.onmyway133.PushNotifications --helper-bundle-id=com.onmyway133.PushNotifications.helper --app-version=1.4.1 --build-version=2 --platform=mas --arch=x64 --icon=Icon/Icon.icns --overwrite
 npx electron-osx-sign "PushNotificationTester-mas-x64/PushNotificationTester.app" --verbose
 npx electron-osx-flat "PushNotificationTester-mas-x64/PushNotificationTester.app" --verbose
